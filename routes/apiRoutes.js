11753959f2592f0d609be9c432822c9a51336e09
Elliot Moffitt <elliot.winton@gmail.com>
2020-11-12 01:31:08 -0500

    apiroutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,28 @@
 const db = require("../db/db.json")
 const store = require("../db/store")
+const router = require("express").Router();
 
 // GET route
-store.getNote()
-// PORT route
+router.get("/notes", (req, res) => {
+    store
+        .getNotes()
+        .then((notes) => res.json(notes))
+        .catch((err) => res.status(500).json(err));
+});
+router.post("/notes", (req, res) => {
+    store
+        .addNotes(req.body)
+        .then((notes) => res.json(notes))
+        .catch((err) => res.status(500).json(err));
+});
 
 // DELETE
+
+router.delete("/notes:id", (req, res) => {
+    store
+        .removeNotes(req.params.id)
+        .then(() => res.json())
+        .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
